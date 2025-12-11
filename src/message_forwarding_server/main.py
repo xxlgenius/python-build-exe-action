ceaf405684eb9958ba0791f39304cd7b0a1206e2
xxlgenius <hszhangown@gmail.com>
2025-12-11 15:09:05 +0800

    v1.0

---
+++ src/message_forwarding_server/main.py
@@ -0,0 +1,12 @@
+# hello.py
+import click
+
+
+@click.command()
+@click.option("--name", default="World", help="Name to greet.")
+def hello(name):
+    click.echo(f"Hello, {name}!")
+
+
+if __name__ == "__main__":
+    hello()
